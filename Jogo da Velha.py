66ef8b32bdef18bbd7aca3145074d27beec3194b
Eduardo <dutakeuchi@gmail.com>
2025-04-15 12:05:24 -0300

    Versao 1.1

--- Jogo da Velha.py
+++ Jogo da Velha.py
@@ -29,7 +29,7 @@ def usuarioNumero(nEscolhidos):
             if 0 < res <10:
                 if res in lista:
                     valid = True
-                    lista[res-1] = 'o'
+                    lista[res-1] = caracUsua
                     nEscolhidos.append(res)
                     print( jogo(lista))
                     print('\n')
@@ -81,7 +81,7 @@ def compuNumero(dificuldade, nEscolhidos, nComputador):
         if len(c) == 1:
             res = c[0]
             if res in lista:
-                lista[res-1] = 'x'
+                lista[res-1] = caracComp
                 nComputador.append(res)
                 print('Turno do computador. Número escolhido', res)
                 print(jogo(lista))
@@ -97,7 +97,7 @@ def compuNumero(dificuldade, nEscolhidos, nComputador):
         res = random.choices(list(range(1,10)),weight, k=1)[0]
         if res in lista:
             valid = True
-            lista[res-1] = 'x'
+            lista[res-1] = caracComp
             lComp.append(res)
             print('Turno do computador. Número escolhido', res)
             print(jogo(lista))
@@ -114,14 +114,41 @@ def compuNumero(dificuldade, nEscolhidos, nComputador):
 ## início do programa
 import random
 
-print ("Jogo da Velha")
-print("Você é o jogador 'o' e o computador é o jogador 'x'")
+print ("Vamos jogar o tradicional jogo da velha?")
+
+## permite ao usuário selecionar com qual caractere será jogado
+valid = False
+while valid == False:
+    caracUsua = input ("Selecione o caractere que deseja utilizar ")
+    if len (caracUsua)!=1:
+        print ("Digite somente um caractere")
+        continue
+    
+    try:
+        caracUsua == int(caracUsua)
+        print ("O caractere nao pode ser um número")
+
+    except ValueError:
+        valid = True
+        print (caracUsua)
+
+## seleciona o caractere do computador
+if caracUsua == 'x':
+    caracComp = 'o'
+else:
+    caracComp = 'x'
+            
 
 ## variável para saber se quer jogar novamente
-jogar = 's'
+jogar = 1
+
+## contagem de vitórias, derrotas e empates
+vitoria = 0
+derrota = 0
+empate = 0
 
 ##enquanto jogar for 's', o jogo será jogado
-while jogar == 's':
+while jogar == 1:
 
     ## relação das posições possíveis de jogar
     lista = list(range(1,10))
@@ -141,6 +168,13 @@ while jogar == 's':
     ## peso para a escolha dos números disponíveis pelo computador
     weight = [1]*9
 
+    ## quantidade de partidas jogadas
+    total = vitoria + derrota + empate
+    
+    print ('\n')
+    print ("Jogo da Velha Rodada", total+1)
+    print("Você é o jogador",caracUsua," e o computador é o jogador",caracComp)
+    print ('\n')
     print(jogo(lista))
     print ('\n')
     
@@ -149,7 +183,7 @@ while jogar == 's':
         dific = input ('Selecione o nível de dificuldade de 1 a 10 ')
         try:
             dific = int(dific)
-            if 0 < dific <= 10:
+            if 0 < dific <= 100:
                 valid = True
                 continue
             else:
@@ -166,11 +200,13 @@ while jogar == 's':
         if ganhador(lUser) == 1:
             print('Parabéns, você ganhou!')
             print('\n')
+            vitoria += 1
             break
 
         if i == 9:
             print('Não houve ganhadores. Jogo empatado!')
             print('\n')
+            empate += 1
             break
         else:
             compuNumero(dific,lUser, lComp)
@@ -179,21 +215,29 @@ while jogar == 's':
             if ganhador(lComp) == 1:
                 print('Você perdeu. O computador ganhou!')
                 print('\n')
+                derrota += 1
                 break
 
     while valid == False:
-        jogar = input ('Deseja jogar novamente? (S ou N) ').lower()
+        jogar = input ('Deseja jogar novamente? Digite 1 para SIM e 2 para NÃO ')
         try:
-            if jogar == 's' or jogar == 'n':
+            jogar = int(jogar)
+            if jogar == 1 or jogar == 2:
                 valid = True
             else:
-                print ('Resposta inválida! Digite S ou N')
+                print ('Resposta inválida! Digite 1 ou 2')
         except:
+            print ('Resposta inválida! Digite um valor válido')
             continue
         
-    print ('\n')
-
+print ('\n')
+print ('Total de jogos: ', total+1)
+print ('Vitórias: ',vitoria)
+print ('Derrotas: ',derrota)
+print ('Empate: ',empate)
 print('Fim de jogo. Obrigado por participar!')
+print ('\n')
+input ('Digite qualquer tecla para sair')
     
             
         
